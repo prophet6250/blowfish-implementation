d7ffbf16578273a2aebbac3616d2af803888bb3b
Kumar Tatsat <rvk8878@gmail.com>
2020-05-07 12:45:07 +0530

    file i/o completed. need to add runtime flags for file names and modes

--- main.c
+++ main.c
@@ -5,29 +5,52 @@
 
 #define KEYSIZE  56
 #define DATASIZE 1024
+#define DATAFILE "data.txt"
+#define KEYFILE  "key.txt"
+#define ENC_FILE "data.eNCrYpT"
 
 int main(int argc, char const *argv[])
 {
 	int i;
-	uint8_t data[DATASIZE], key[KEYSIZE], *encrypted;
-	
-	printf("enter the data (max length 1024 characters)\n");
-	fgets(data, DATASIZE, stdin);
+	uint8_t data[DATASIZE], key[KEYSIZE], character, *encrypted;
+	//uint8_t *decrypted;
 
-	printf("enter key (max length = 56 characters)\n");
-	fgets(key, KEYSIZE, stdin);
-	
-	for (i = 0; i < strlen(data); i++) {
-		printf("%.2x ", data[i]);
+	FILE *dataF = fopen(DATAFILE, "r+");
+	FILE *keyF  = fopen(KEYFILE, "r");
+	FILE *encF  = fopen(ENC_FILE, "w+");
+
+	/* reading from the data file */
+	i = 0;
+	while (!feof(dataF)) {
+		character = fgetc(dataF);
+
+		printf("%c", character);
+		data[i] = character;
+		i += 1;
 	}
-	printf("\n");
+	clearerr(dataF);
+	printf("reading from the datafile done\n");
+
+	/* reading from the key file */
+	i = 0;
+	while (!feof(keyF)) {
+		character = fgetc(keyF);
+		
+		printf("%c", character);
+		key[i] = character;
+		i += 1;
+	}
+	clearerr(keyF);
+	printf("reading from the keyfile done\n");
+
 	encrypted = blowfish_initialize(data, key);
+	fputs(encrypted, encF);
+	printf("writing to the encrypted file done\n");
+	clearerr(encF);
 
-	printf("encrypted data is:\n");
-	for (i = 0; i < strlen(data); i++) {
-		printf("%.2x ", encrypted[i]);
-	}
+	fclose(dataF);
+	fclose(keyF);
+	fclose(encF);
 
-	printf("\n");
 	return 0;
 }
